04477cbc7e03adb95a9d7b69408f5cad2140c08c
Andres Gutierrez <andresg206@gmail.com>
2023-03-23 14:39:12 -0300

    Archivos básicos del theme

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php 
+/*
+ *  Author: Andres Gutierrez | @agutcode
+ *  URL: agutcode.com | @agutcode
+ *  Custom functions, support, custom post types and more.
+ */
---
+++ header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+
+		<link href="//www.google-analytics.com" rel="dns-prefetch">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="<?php bloginfo('description'); ?>">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
